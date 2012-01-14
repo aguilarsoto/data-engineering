776f98d3df1b4f4a01e4479f6b638625f434fbcf
Marco V. Aguilar Soto <aguilarsoto@gmail.com>
2012-01-14 10:30:08 -0500

    living social test first commit start of a new rails repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DataEngineering::Application.config.secret_token = '8a0def9e1ad467336102fbe05f46825bbb6887f24841cc6f77c9e526bc7393c00761e39689873aa77280eb08c9032afac7bf26869ce2615b94acbfb613ab7525'
